63ad847dc5edc3a6094c9cc17fe98b8a151fa690
Edward <edward_song@yeah.net>
2018-11-20 16:44:03 +0800

    create program

---
+++ AutoBatchForProductionLine/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using Edward;
+
+namespace AutoBatchForProductionLine
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new frmMain());
+
+            SplashForm.StartSplash(@".\splash.bmp", Color.FromArgb(128, 128, 128));
+            // simulating operations at startup
+            System.Threading.Thread.Sleep(1000);
+            // close the splash screen
+            SplashForm.CloseSplash();
+
+        }
+    }
+}
