cfbd95ce862f9ca6228bcf981f515a7f55a6e471
Edward <edward_song@yeah.net>
2018-11-21 15:51:08 +0800

    create ini

--- AutoBatchForProductionLine/p.cs
+++ AutoBatchForProductionLine/p.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using Edward;
 
 namespace AutoBatchForProductionLine
 {
@@ -33,6 +35,13 @@ namespace AutoBatchForProductionLine
         public static string CMSV6IP = string.Empty;
         public static string CMSV6Port = string.Empty;
         public static string CMSV6ReportTime = string.Empty;
+        //GB28181
+        public static string GB2_ServIP = string.Empty;
+        public static string GB2_ServPort = string.Empty;
+        public static string GB2_DevNo = string.Empty;    //设备ID
+        public static string GB2_ChnNo = string.Empty;   //设备通道ID
+        public static string GB2_ServNo = string.Empty; //服务器ID
+        public static string GB2_Passwd = string.Empty;
         //NetCheck
         public static string NetCheckIP = "127.0.0.1";
         public static string NetCheckPort = "554";
@@ -48,6 +57,56 @@ namespace AutoBatchForProductionLine
         /// </summary>
         public static void CreateFolder()
         {
+            if (!Directory.Exists(AppFolder))
+                Directory.CreateDirectory(AppFolder);
+
+        }
+
+        public static void CreateIni()
+        {
+            IniFile.iniFilePathValue = IniPath;
+            if (!File.Exists(IniPath))
+            {
+                IniFile.CreateIniFile(IniPath);
+                //SysConfig
+                IniFile.IniWriteValue("SysConfig", "Version", Application.ProductVersion);
+                IniFile.IniWriteValue("SysConfig", "CurrentDevice", CurrentDevice);
+                IniFile.IniWriteValue("SysConfig", "SyncTime", SyncTime);
+                IniFile.IniWriteValue("SysConfig", "SetWiFi", SetWiFi);
+                IniFile.IniWriteValue("SysConfig", "SetAPN", SetAPN);
+                IniFile.IniWriteValue("SysConfig", "SetCMSV6", SetCMSV6);
+                IniFile.IniWriteValue("SysConfig", "SetGB28181", SetGB28181);
+                IniFile.IniWriteValue("SysConfig", "SetCheckNet",SetCheckNet);
+                IniFile.IniWriteValue("SysConfig", "SetGPS", SetGPS);
+                IniFile.IniWriteValue("SysConfig", "SetPowerOff", SetPowerOff);
+                //
+                IniFile.IniWriteValue("WiFi", "WiFiSSID", WiFiSSID);
+                IniFile.IniWriteValue("WiFi", "WiFiSSID", WiFiPwd);
+                //
+                IniFile.IniWriteValue("APN", "APN", APN);
+                IniFile.IniWriteValue("APN", "APNUser", APNUser);
+                IniFile.IniWriteValue("APN", "APNPwd", APNPwd);
+                //
+                IniFile.IniWriteValue("CMSV6", "CMSV6IP", CMSV6IP);
+                IniFile.IniWriteValue("CMSV6", "CMSV6Port", CMSV6Port);
+                IniFile.IniWriteValue("CMSV6", "CMSV6ReportTime", CMSV6ReportTime);
+                //
+                IniFile.IniWriteValue("GB28181", "GB2_ServIP", GB2_ServIP);
+                IniFile.IniWriteValue("GB28181", "GB2_ServPort", GB2_ServPort);
+                IniFile.IniWriteValue("GB28181", "GB2_DevNo", GB2_DevNo);
+                IniFile.IniWriteValue("GB28181", "GB2_ChnNo", GB2_ChnNo);
+                IniFile.IniWriteValue("GB28181", "GB2_ServNo", GB2_ServNo);
+                IniFile.IniWriteValue("GB28181", "GB2_Passwd", GB2_Passwd);
+                //
+                IniFile.IniWriteValue("NetCheck", "NetCheckIP", NetCheckIP);
+                IniFile.IniWriteValue("NetCheck", "NetCheckPort", NetCheckPort);
+                //
+                IniFile.IniWriteValue("GPS", "GPS", GPS);
+                //
+                IniFile.IniWriteValue("PowerOff", "PowerOff", PowerOff);
+
+
+            }
 
         }
 
